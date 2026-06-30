f9625588284c333d5abec587471bbdf745329bc8
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-30 19:54:30 +0300

    MemberView buit

--- team/serializers.py
+++ team/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Member
 
-class TeamSerializer(serializers.ModelSerializer):
+class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = "__all__"
--- team/views.py
+++ team/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from .serializers import MemberSerializer
+from .models import Member
+
+class MemberView(ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
