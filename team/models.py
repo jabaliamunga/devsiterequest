b4269fa20c864b6fb6b9628584988a5a6ac55b43
codewithshadyy <kipkoechshadrack10@gmail.com>
2026-06-30 19:51:47 +0300

    created the serializer.py file and validated Member Model

--- team/models.py
+++ team/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Member(models.Model):
     name = models.CharField(max_length=200)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     image = models.ImageField(upload_to="member/")
     awards = models.JSONField(default=list)
     roles = models.JSONField(default=list)
